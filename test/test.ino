eb9816b9751a11f73bf2a2e225fe5a930bafc6ae
zomint <zomint01@163.com>
2024-06-01 16:57:59 +0800

    update

--- test/test.ino
+++ test/test.ino
@@ -6,8 +6,13 @@
 
 namespace {
 
+template <typename X>
+inline void Log(const X& x) {
+  Serial.println(time::Now() + ' ' + x);
+}
+
 void InfiniteLoopOnFailure(const char* function, const uint32_t line_number) {
-  Serial.println(String(F("entering an infinite loop due to failure in ")) + function + ", at line number: " + line_number);
+  Log(String(F("entering an infinite loop due to failure in ")) + function + ", at line number: " + line_number);
   while (true) {
     yield();
   }
@@ -17,12 +22,13 @@ void InfiniteLoopOnFailure(const char* function, const uint32_t line_number) {
 
 void setup() {
   Serial.begin(115200);
-  Serial.println(time::Now() + F(" setup"));
+  Log(F("setup"));
   Wire.begin();
-  Serial.println(time::Now() + F(" setup successful"));
+  // do something
+  Log(F("setup successful"));
 }
 
 void loop() {
-  Serial.println(time::Now() + F(" loop"));
+  Log(F("loop"));
   delay(500);
 }
---
+++ test/time_utils.cpp
@@ -0,0 +1,16 @@
+#include "time_utils.h"
+
+namespace time {
+String Now() {
+  const auto now = millis();
+  const uint16_t millis = now % 1000;
+  const uint16_t seconds = now / 1000 % 60;
+  const uint16_t minutes = now / 60000 % 60;
+  const uint16_t hours = now / 3600000 % 100;
+
+  static const String s_format(F("%02" PRIu16 ":%02" PRIu16 ":%02" PRIu16 ".%03" PRIu16));
+  char buffer[13];
+  snprintf(buffer, sizeof(buffer), s_format.c_str(), hours, minutes, seconds, millis);
+  return String(buffer);
+}
+}  // namespace time
